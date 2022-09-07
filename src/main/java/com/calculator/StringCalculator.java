71bf762df53b0e6b2ce3d098f028d47cfb5f7a07
Aniket Satish Mane <aniketsmd310@gmail.com>
2022-09-07 20:48:49 +0530

    Refactored code v1

--- src/main/java/com/calculator/StringCalculator.java
+++ src/main/java/com/calculator/StringCalculator.java
@@ -1,9 +1,19 @@
 package com.calculator;
 
+import java.util.*;
+import java.util.stream.Collectors;
+
 public class StringCalculator {
     int add(String num){
+        int sum=0;
+        if(num.length()!=0){
+           List<Integer>list= Arrays.stream(num.split(",")).map(Integer::parseInt).collect(Collectors.toList());
+           sum=list.stream().reduce(Integer::sum).orElseThrow();
+        }
 
-        return 0;
+        return sum;
     }
+
+
 
 }
