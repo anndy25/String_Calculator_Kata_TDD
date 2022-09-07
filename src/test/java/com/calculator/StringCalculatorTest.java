0ee010af9aa632fdb64c6db01c935d48bcf9f69a
Aniket Satish Mane <aniketsmd310@gmail.com>
2022-09-07 20:54:45 +0530

    Completed test cases of task 1

--- src/test/java/com/calculator/StringCalculatorTest.java
+++ src/test/java/com/calculator/StringCalculatorTest.java
@@ -13,6 +13,16 @@ class StringCalculatorTest {
     void should_return_zero_for_empty_string(){
           assertEquals(0,stringCalculator.add(""));
     }
+
+    //Task 1:
+    //TODO:- The method can take numbers as a string separated by comma and will return their sum
+    @Test
+    void should_return_sum_for_two_number() {
+        assertEquals(5,stringCalculator.add("2,3"));
+        assertEquals(10,stringCalculator.add("5,5"));
+        assertEquals(45,stringCalculator.add("45,0"));
+    }
+
     
 
 }
