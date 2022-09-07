754ca7cb86adbb9541259524be70d29beb1f7ff5
Aniket Satish Mane <aniketsmd310@gmail.com>
2022-09-07 19:30:42 +0530

    Completed test case for task 0

--- src/test/java/com/calculator/StringCalculatorTest.java
+++ src/test/java/com/calculator/StringCalculatorTest.java
@@ -1,8 +1,18 @@
 package com.calculator;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class StringCalculatorTest {
+    StringCalculator stringCalculator =new StringCalculator();
+
+    // Task 0:
+    // TODO:-  for an empty string, it will return 0
+    @Test
+    void should_return_zero_for_empty_string(){
+          assertEquals(0,stringCalculator.add(""));
+    }
     
 
 }
